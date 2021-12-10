592c4107b54717b94c1fe0f5bde2bed2f4ffb9bc
ISHITSUKA Hikaru <ishitsuka.hikaru@aist.go.jp>
2021-12-10 16:04:05 +0900

    add emacs keybind, window move

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,10 @@
 (column-number-mode t)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
+
+;; window move
+;; ref https://www.emacswiki.org/emacs/WindMove
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
